67748a2963f84153dd93ee1146dcf088214d308a
DaviLimaPereira <39305143+DaviLimaPereira@users.noreply.github.com>
2023-11-19 00:04:19 +0000

    Modificações 08
    Exclusão de arquivos xml desnecessarios, adição de pages na pasta abelha rainha e melhorias em RouteSwitch.php e no arquivo index.html da pasta abelha rainha.

--- RouteSwitch.php
+++ RouteSwitch.php
@@ -6,36 +6,46 @@
       require __DIR__ . '/views/home.html';
     }
 
-    //rota para a listagem de colaboradoras
-    protected function colaboradoras(){
-      require __DIR__ . '/views/colaboradoras/index.html';
-    }
-
-    //rota para a listagem de clientes
-    protected function clientes(){
-      require __DIR__ . '/views/clientes/index.html';
-    }
-
+    /*----------------------------------------------------------*/
     //rota para a page principal relacionada aos cosméticos da Abelha Rainha
     protected function abelhaRainha(){
       require __DIR__ . '/views/abelha_rainha/index.html';
     }
 
+    //rota para cadastrar um novo pedido da Abelha Rainha
+    protected function novoPedidoAbelhaRainha(){
+      require __DIR__ . '/views/abelha_rainha/novoPedido.html';
+    }
+
+    //rota para exibir pedido da Abelha Rainha
+    protected function exibirPedidoAbelhaRainha(){
+      require __DIR__ . '/views/abelha_rainha/exibirPedido.html';
+    }
+
+    //rota para exibir detalhes do pedido da Abelha Rainha
+    protected function exibirDetalhesPedidoAbelhaRainha(){
+      require __DIR__ . '/views/abelha_rainha/exibirDetalhes.html';
+    }
+
+    //--------------------------------------------------------------
     //rota para a page principal relacionada aos cosméticos da Avon
     protected function avon(){
       require __DIR__ . '/views/avon/index.html';
     }
 
+    //---------------------------------------------------------------
     //rota para a page principal relacionada aos cosméticos da Eudora
     protected function eudora(){
       require __DIR__ . '/views/eudora/index.html';
     }
 
+    /*------------------------------------------------------------*/
     //rota para a page principal relacionada aos cosméticos da Natura
     protected function natura(){
       require __DIR__ . '/views/natura/index.html';
     }
-    
+
+    /*------------------------------------------------------------*/
     //rota para pages não encotradas
     protected function __call($name, $arguments){
       http_response_code(404);
